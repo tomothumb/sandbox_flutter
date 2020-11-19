1d07f070faae7395468a3e18b54b98e37e54ad63
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-11-20 02:41:38 +0900

    tutorial: using package

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart'
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +7,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'welcome to flutter',
       home: Scaffold(
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-            child: Text('Hello world!')
+            child: Text(wordPair.asPascalCase)
         )
       )
     );
