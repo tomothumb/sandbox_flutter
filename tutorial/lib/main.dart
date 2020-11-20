a2c23cd4c336f6eb96f850e3bb5fc41c1ae9580b
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-11-20 17:39:53 +0900

    tutorial: card layout

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -1,213 +1,127 @@
 import 'package:flutter/material.dart';
 // import 'package:english_words/english_words.dart';
 
-class Product {
-  const Product({this.name});
-  final String name;
-}
 
 void main() {
-  runApp(MaterialApp(
-    title: 'My app',
-    home: ShoppingList(
-      products: <Product>[
-        Product(name: 'Eggs'),
-        Product(name: 'Flour'),
-        Product(name: 'Chocolate chips'),
-      ]
-    ),
-  ));
+  runApp(MyApp());
 }
 
-class ShoppingList extends StatefulWidget {
-  ShoppingList({Key key, this.products}) : super(key: key);
-
-  final List<Product> products;
-
-  @override
-  _ShoppingListState createState() => _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-
-  Set<Product> _shoppingCart = Set<Product>();
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState((){
-      if (!inCart){
-        _shoppingCart.add(product);
-      }else{
-        _shoppingCart.remove(product);
-      }
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shopping List'),
+    Color color = Theme
+        .of(context)
+        .primaryColor;
+
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical:10.0),
-        children: widget.products.map((Product product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged,
-          );
-        }).toList(),
-      )
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
 
-typedef void CartChangedCallback(Product product, bool inCart);
-
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({this.product, this.inCart, this.onCartChanged})
-    : super(key: ObjectKey(product));
-
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  Color _getColor(BuildContext context){
-    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
-  }
-
-  TextStyle _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-    return TextStyle(
-      color: Colors.black54,
-      decoration: TextDecoration.lineThrough,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCartChanged(product, inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
-      ),
-      title: Text(product.name, style: _getTextStyle(context)),
-    );
-  }
-}
-
-
-
-class TutorialHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation menu",
-            onPressed: null,
-          ),
-          title: Text("Example"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'search',
-              onPressed: null,
-            )
-          ]),
-      body: Center(
-        child: Column(
-          children: [
-            Text("aaaaa"),
-            Counter(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add',
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        print('tapped');
-      },
-      child: Container(
-        height: 30.0,
-        padding: const EdgeInsets.all(15.0),
-        margin:  const EdgeInsets.symmetric(horizontal: 10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightGreen[500],
-        ),
-        child: Center(
-          child: Text('Engage'),
-        )
-      ),
-    );
-  }
-}
-
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({this.count});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text('Increment'),
-    );
-  }
-}
-
-
-class Counter extends StatefulWidget {
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState((){
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(10.0),
+    child: Row(
       children: [
-        CounterIncrementor(onPressed: _increment,),
-        CounterDisplay(count: _counter)
+        Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Text('bbbbbbbb',
+                        style: TextStyle(fontWeight: FontWeight.bold)
+                    )
+                ),
+                Text('xxxxxxxxxx',
+                    style: TextStyle(color: Colors.grey[500]))
+              ],
+            )
+        ),
+        Icon(Icons.star, color: Colors.red[500],),
+        Text('41'),
       ],
+    ),
+  );
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+              margin: const EdgeInsets.only(top: 10.0),
+              child: Text(label,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: color,
+                  )
+              )
+          ),
+        ]
+    );
+  }
+
+  Widget buttonSection(context) {
+    Color color = Theme
+        .of(context)
+        .primaryColor;
+
+    return Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildButtonColumn(color, Icons.call, 'Call'),
+            _buildButtonColumn(color, Icons.near_me, 'Route'),
+            _buildButtonColumn(color, Icons.share, 'Share'),
+          ],
+        )
+    );
+  }
+
+  Widget textSection = Container(
+      padding: const EdgeInsets.all(30),
+      child: Text(
+        'asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfasasdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas asdfasfasf asfasfa sadfasd asdfasdfas fsf asfasdfasdf asdfas',
+        softWrap: true,
+      )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Flutter Layout Demo')),
+        body: ListView(
+            children: [
+              Image.asset(
+                'images/sample.jpg',
+                width: 300,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection(context),
+              textSection
+            ]
+        )
     );
   }
 }
-
