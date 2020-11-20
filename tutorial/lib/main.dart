28a8da6b0a9915b0cbe87d341a050b7e9c96b69c
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-11-20 14:35:49 +0900

    fmt

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -4,20 +4,18 @@ import 'package:english_words/english_words.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: RandomWords()
-    );
+        title: 'Startup Name Generator',
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
+        home: RandomWords());
   }
 }
-
 
 class RandomWords extends StatefulWidget {
   @override
@@ -29,93 +27,75 @@ class _RandomWordsState extends State<RandomWords> {
   final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 12.0);
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
       padding: EdgeInsets.all(10),
-      itemBuilder: (context,i){
-        if(i.isOdd){
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
           return Divider();
         }
         final index = i ~/ 2; //
-        if (index >= _suggestions.length){
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
       },
     );
   }
+
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState((){
-          if (alreadySaved){
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
+        title: Text(pair.asPascalCase, style: _biggerFont),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
         });
-      }
-    );
   }
 
-
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-              (WordPair pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  )
-                );
-              }
-          );
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map((WordPair pair) {
+        return ListTile(
+            title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ));
+      });
 
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
 
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions')
-            ),
-            body: ListView(children: divided)
-          );
-
-        }
-      )
-    );
+      return Scaffold(
+          appBar: AppBar(title: Text('Saved Suggestions')),
+          body: ListView(children: divided));
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions()
-    );
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+          actions: [
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          ],
+        ),
+        body: _buildSuggestions());
   }
-
 }
-
-
 
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
