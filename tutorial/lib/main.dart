cadae33fb331df8428f5f1275ec5e8af993cebed
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-11-20 14:13:12 +0900

    tutorial: clickable

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -23,6 +23,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 12.0);
 
   Widget _buildSuggestions(){
@@ -41,11 +42,25 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont
-      )
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState((){
+          if (alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 
