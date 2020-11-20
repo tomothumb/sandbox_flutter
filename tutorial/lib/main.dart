e1b90185936635781b1821b8823c7595bbded619
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-11-20 15:19:18 +0900

    tutorial: stateful counter

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -27,13 +27,92 @@ class TutorialHome extends StatelessWidget {
             )
           ]),
       body: Center(
-        child: Text("aaaaa"),
+        child: Column(
+          children: [
+            Text("aaaaa"),
+            Counter(),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add',
         child: Icon(Icons.add),
         onPressed: null,
       ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        print('tapped');
+      },
+      child: Container(
+        height: 30.0,
+        padding: const EdgeInsets.all(15.0),
+        margin:  const EdgeInsets.symmetric(horizontal: 10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text('Engage'),
+        )
+      ),
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
+
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState((){
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CounterIncrementor(onPressed: _increment,),
+        CounterDisplay(count: _counter)
+      ],
     );
   }
 }
